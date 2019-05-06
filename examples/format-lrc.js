0ac9de23838862593d07a61777ead3fab370aeef
Curtis Elsasser <curt.elsasser@gmail.com>
2019-05-05 22:23:30 -0400

    - add some field and literal format and parsing examples. - revealed more areas where where we need to escape reserved regex characters. - also revealed deficiency with supporting floating point precision. Remove support all together. - move escape functionality to _utils so that we may test it separately - update readme with revisions and add examples

--- examples/format-lrc.js
+++ examples/format-lrc.js
@@ -6,17 +6,17 @@
 
 const run=require("./_run");
 
-run.format("lrc - padding=' ', width=10, paths=default",
+run.format('left/right/center - padding=" ", width=10',
 	"${10l}|${10c}|${10r}",
 	["left", "center", "right"]
 );
 
-run.format("lrc - padding='.', width=15, paths=indexes",
+run.format('left/right/center - padding=".", paths=indexes, width=15',
 	"${0:.15l}|${1:.15c}|${0:.15r}",
 	["element-0", "element-1"]
 );
 
-run.format("lrc - padding='.', width=15, paths=object",
+run.format('left/right/center - padding=".", paths=object, width=15',
 	"${left:.15l}|${center:.15c}|${right:.15r}",
 	{
 		center: "dead-eye",
--- examples/parse-lrc.js
+++ examples/parse-lrc.js
@@ -6,17 +6,17 @@
 
 const run=require("./_run");
 
-run.parse("lrc - padding=' ', width=10, paths=default",
+run.parse('left/right/center - padding=" ", width=10',
 	"${10l}|${10c}|${10r}",
 	"left      |  center  |     right"
 );
 
-run.parse("lrc - padding='.', width=15, paths=indexes",
+run.parse('left/right/center - padding=".", paths=indexes, width=15',
 	"${0:.15l}|${1:.15c}|${0:.15r}",
 	"element-0......|...element-1...|......element-0"
 );
 
-run.parse("lrc - padding='.', width=15, paths=object",
+run.parse('left/right/center - padding=".", paths=object, width=15',
 	"${left:.15l}|${center:.15c}|${right:.15r}",
 	"leftie.........|...dead-eye....|........rightie"
 );
